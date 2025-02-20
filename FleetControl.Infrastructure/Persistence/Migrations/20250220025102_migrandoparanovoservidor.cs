bad759083d3112a31b955585dcf9c21eff118bb3
Rodrigo Santos <rodrigocbs914@gmail.com>
2025-02-20 00:27:52 -0300

    feat: realizando nova migration para a nova instância do SqlServer

---
+++ FleetControl.Infrastructure/Persistence/Migrations/20250220025102_migrandoparanovoservidor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FleetControl.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class migrandoparanovoservidor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
